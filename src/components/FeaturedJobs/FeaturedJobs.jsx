df0282df1c7f2aefa9122eb9580fe59e5259c035
jnakht <jisan.cse.cu@gmail.com>
2025-03-06 13:18:31 +0600

    handle show more button

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -9,12 +9,19 @@ const FeaturedJobs = () => {
         .then(res => res.json())
         .then(data => setJobs(data))
     } ,[])
+    const [dataLength, setDataLength] = useState(4);
     return (
-        <div className="grid grid-cols-2 gap-6">
+        <div>
+            <div className="grid grid-cols-2 gap-6">
             {
-                jobs.map(job => <Job job={job}></Job>)
+                jobs.slice(0, dataLength).map(job => <Job job={job}></Job>)
             }
+            </div>
+            <div className={`mx-auto border-2 border-green-500 text-center mt-2 ${dataLength === jobs.length && 'hidden'}`}>
+                <button onClick={() => setDataLength(jobs.length)} className="btn btn-primary ">Show All</button>
+            </div>
         </div>
+        
     );
 };
 
