a7851bec10618bf119cc76fdfc94feba3548659d
jnakht <jisan.cse.cu@gmail.com>
2025-03-06 20:05:01 +0600

    separated jobs that are on local storage

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,6 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import { getApplicants } from "../Utility/Utility";
+import { useEffect } from "react";
 
 
 const AppliedJobs = () => {
+    // all jobs
+    const allJobs = useLoaderData();
+    // id stored on local storage
+    const ids = getApplicants();
+    useEffect( () => {
+        const appliedJobs = allJobs.map(job => {
+            if(ids.includes(job.id)) return job;
+        })
+        console.log('these are the applied jobs: ', appliedJobs)
+    } ,[])
     return (
         <div>
             <h2>Jobs i applied</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/appliedJobs',
-        element: <AppliedJobs></AppliedJobs>
+        element: <AppliedJobs></AppliedJobs>,
+        loader: () => fetch('jobs.json')
       },
       {
         path: '/job/:id',
