19fa22d124e4abd746d8c6444740c90abeade212
jnakht <jisan.cse.cu@gmail.com>
2025-03-06 20:39:40 +0600

    applied jobs showed on Applied JObs route

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import { getApplicants } from "../Utility/Utility";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 const AppliedJobs = () => {
@@ -8,15 +8,25 @@ const AppliedJobs = () => {
     const allJobs = useLoaderData();
     // id stored on local storage
     const ids = getApplicants();
+    const [jobsToShow, setJobsToShow] = useState([]);
     useEffect( () => {
         const appliedJobs = allJobs.filter(job => {
             if(ids.includes(job.id)) return job;
         })
+        setJobsToShow(appliedJobs);
         console.log('these are the applied jobs: ', appliedJobs)
     } ,[])
     return (
         <div>
-            <h2>Jobs i applied</h2>
+            <h2>Jobs i applied: {jobsToShow.length}</h2>
+            <div>
+                {
+                    jobsToShow.map(job => <div> 
+                        <h2>{job.job_title}, {job.company_name}</h2>
+                        
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
