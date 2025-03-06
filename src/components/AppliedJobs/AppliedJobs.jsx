842cc5176f298c8e970b4cac034ee8afdfbd8639
jnakht <jisan.cse.cu@gmail.com>
2025-03-06 20:48:23 +0600

    dropdown for filter

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -9,21 +9,31 @@ const AppliedJobs = () => {
     // id stored on local storage
     const ids = getApplicants();
     const [jobsToShow, setJobsToShow] = useState([]);
-    useEffect( () => {
+    useEffect(() => {
         const appliedJobs = allJobs.filter(job => {
-            if(ids.includes(job.id)) return job;
+            if (ids.includes(job.id)) return job;
         })
         setJobsToShow(appliedJobs);
         console.log('these are the applied jobs: ', appliedJobs)
-    } ,[])
+    }, [])
     return (
         <div>
             <h2>Jobs i applied: {jobsToShow.length}</h2>
             <div>
+                <details className="dropdown">
+                    <summary className="btn m-1">Filter</summary>
+                    <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a>All</a></li>
+                        <li><a>Remote</a></li>
+                        <li><a>Onsite</a></li>
+                    </ul>
+                </details>
+            </div>
+            <div>
                 {
-                    jobsToShow.map(job => <div> 
+                    jobsToShow.map(job => <div>
                         <h2>{job.job_title}, {job.company_name}</h2>
-                        
+
                     </div>)
                 }
             </div>
